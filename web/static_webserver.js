de558524dbf7a09eb4cc676ee391609a36afb147
Flavio <flvribeiro27@gmail.com>
2017-11-16 13:52:29 -0300

    Node para web

---
+++ web/static_webserver.js
@@ -0,0 +1,37 @@
+const http = require('http')
+const fs = require('fs')
+const base = 'examples/public_html'
+
+http.createServer((req, res) => {
+    res.setHeader('Content-Type', 'text/html')
+
+    let pathname = base + req.url
+
+    fs.stat(pathname, (err, stats) => {
+        if (err) {
+            console.log(err)
+            res.writeHead(404)
+            res.write('Resource missing 404\n')
+            
+            return
+        }
+
+        res.setHeader('Content-Type', 'text/html')
+        
+        let file = fs.createReadStream(pathname)
+        
+        file.on('open', () => {
+            res.statusCode = 200
+            file.pipe(res)
+        })
+    
+        file.on('error', err => {
+            res.writeHead(403)
+            res.write('file missing or permission deniend')
+            console.log(err)
+        } )
+    })
+
+}).listen(8124)
+
+console.log('Server running at 8124')
